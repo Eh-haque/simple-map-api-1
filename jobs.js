2a167363e959212cf1f8f427744c0056a2916484
Eh-haque <ehtisamul@gmail.com>
2022-10-07 21:23:17 +0600

    afk fxv minute

--- jobs.js
+++ jobs.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 let url =
     "https://gisweb.casey.vic.gov.au/IntraMaps90/ApplicationEngine/frontend/mapbuilder/yourproperty.htm?configId=243fbf74-7d66-4208-899d-91b1d08ff8bf&liteConfigId=b2af2973-160e-4664-8e96-fe701aeaa67f&title=WW91ciBQcm9wZXJ0eSBhbmQgUGxhbm5pbmc%3D";
 
-schedule.scheduleJob("*/2 * * * *", async function () {
+schedule.scheduleJob("*/30 * * * *", async function () {
     await fs.unlinkSync("./tracing.json");
 
     const browser = await Puppeteer.launch({ devtools: true });
