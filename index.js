b9e827788e2cd2155f9fc106c6258d1391cd4023
Eh-haque <ehtisamul@gmail.com>
2022-10-07 21:20:21 +0600

    afk f

--- index.js
+++ index.js
@@ -2,51 +2,48 @@ const express = require("express");
 const puppeteer = require("puppeteer");
 const tracingData = require("./tracing.json");
 const closest_match = require("closest-match");
-const fs = require("fs");
+
 require("./jobs");
 
 const app = express();
 
-let url =
-    "https://gisweb.casey.vic.gov.au/IntraMaps90/ApplicationEngine/frontend/mapbuilder/yourproperty.htm?configId=243fbf74-7d66-4208-899d-91b1d08ff8bf&liteConfigId=b2af2973-160e-4664-8e96-fe701aeaa67f&title=WW91ciBQcm9wZXJ0eSBhbmQgUGxhbm5pbmc%3D";
+// async function appStart() {
+//     const browser = await puppeteer.launch({ devtools: true });
+//     const page = await browser.newPage();
+//     await page.tracing.start({
+//         categories: ["devtools.timeline"],
+//         path: "tracing.json",
+//     });
+//     await page.goto(url, { waitUntil: "load", timeout: 0 });
+//     // await page.screenshot({ path: "example.png" });
 
-async function appStart() {
-    const browser = await puppeteer.launch({ devtools: true });
-    const page = await browser.newPage();
-    await page.tracing.start({
-        categories: ["devtools.timeline"],
-        path: "tracing.json",
-    });
-    await page.goto(url, { waitUntil: "load", timeout: 0 });
-    // await page.screenshot({ path: "example.png" });
+//     await page.tracing.stop();
 
-    await page.tracing.stop();
+//     await browser.close();
+// }
 
-    await browser.close();
-}
+// app.post("/start_app", async (req, res) => {
+//     try {
+//         // await fs.unlinkSync("./tracing.json");
+//         appStart();
+//         res.status(200).send({ message: "App started" });
 
-app.post("/start_app", async (req, res) => {
-    try {
-        // await fs.unlinkSync("./tracing.json");
-        appStart();
-        res.status(200).send({ message: "App started" });
+// const browser = await puppeteer.launch({ devtools: true });
+// const page = await browser.newPage();
+// await page.tracing.start({
+//     categories: ["devtools.timeline"],
+//     path: "tracing.json",
+// });
+// await page.goto(url);
+// // await page.screenshot({ path: "example.png" });
 
-        // const browser = await puppeteer.launch({ devtools: true });
-        // const page = await browser.newPage();
-        // await page.tracing.start({
-        //     categories: ["devtools.timeline"],
-        //     path: "tracing.json",
-        // });
-        // await page.goto(url);
-        // // await page.screenshot({ path: "example.png" });
+// await page.tracing.stop();
 
-        // await page.tracing.stop();
-
-        // await browser.close();
-    } catch (error) {
-        res.status(404).send({ message: "Some thing went wrong", error });
-    }
-});
+// await browser.close();
+//     } catch (error) {
+//         res.status(404).send({ message: "Some thing went wrong", error });
+//     }
+// });
 
 app.get("/", async (req, res) => {
     try {
@@ -62,7 +59,6 @@ app.get("/", async (req, res) => {
                 eventUrl.includes("IntraMapsSession=") &&
                 !eventUrl.includes("IntraMapsSession=null")
             ) {
-                console.log(eventUrl);
                 const substring = eventUrl.substring(
                     eventUrl.indexOf("IntraMapsSession=") + 17
                 );
@@ -80,5 +76,5 @@ app.get("/", async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server listening on port", PORT));
--- jobs.js
+++ jobs.js
@@ -1,5 +1,23 @@
 const schedule = require("node-schedule");
+const Puppeteer = require("puppeteer");
+const fs = require("fs");
 
-schedule.scheduleJob("*/30 * * * *", function () {
-    console.log("The answer to life, the universe, and everything!");
+let url =
+    "https://gisweb.casey.vic.gov.au/IntraMaps90/ApplicationEngine/frontend/mapbuilder/yourproperty.htm?configId=243fbf74-7d66-4208-899d-91b1d08ff8bf&liteConfigId=b2af2973-160e-4664-8e96-fe701aeaa67f&title=WW91ciBQcm9wZXJ0eSBhbmQgUGxhbm5pbmc%3D";
+
+schedule.scheduleJob("*/2 * * * *", async function () {
+    await fs.unlinkSync("./tracing.json");
+
+    const browser = await Puppeteer.launch({ devtools: true });
+    const page = await browser.newPage();
+    await page.tracing.start({
+        categories: ["devtools.timeline"],
+        path: "tracing.json",
+    });
+    await page.goto(url, { waitUntil: "load", timeout: 0 });
+    // await page.screenshot({ path: "example.png" });
+
+    await page.tracing.stop();
+
+    await browser.close();
 });
