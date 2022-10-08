976b7735ce7b49462adcbfafe107917ce0f49644
Eh-haque <ehtisamul@gmail.com>
2022-10-09 02:08:20 +0600

    again

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-
-// const tracingData = require("./tracing.json");
-// require("./jobs");
-
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = require("express")();
 
 let chrome = {};
 let puppeteer;
-
-let url =
-    "https://gisweb.casey.vic.gov.au/IntraMaps90/ApplicationEngine/frontend/mapbuilder/yourproperty.htm?configId=243fbf74-7d66-4208-899d-91b1d08ff8bf&liteConfigId=b2af2973-160e-4664-8e96-fe701aeaa67f&title=WW91ciBQcm9wZXJ0eSBhbmQgUGxhbm5pbmc%3D";
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     chrome = require("chrome-aws-lambda");
@@ -46,7 +35,10 @@ app.get("/api", async (req, res) => {
             categories: ["devtools.timeline"],
         });
 
-        await page.goto(url, { waitUntil: "load", timeout: 0 });
+        await page.goto(
+            "https://gisweb.casey.vic.gov.au/IntraMaps90/ApplicationEngine/frontend/mapbuilder/yourproperty.htm?configId=243fbf74-7d66-4208-899d-91b1d08ff8bf&liteConfigId=b2af2973-160e-4664-8e96-fe701aeaa67f&title=WW91ciBQcm9wZXJ0eSBhbmQgUGxhbm5pbmc%3D",
+            { waitUntil: "load", timeout: 0 }
+        );
 
         const tracing = JSON.parse(await page.tracing.stop());
 
@@ -66,6 +58,7 @@ app.get("/api", async (req, res) => {
                 const substring = eventUrl.substring(
                     eventUrl.indexOf("IntraMapsSession=") + 17
                 );
+                console.log(substring);
 
                 return res.status(200).send({
                     title: await page.title(),
@@ -85,66 +78,24 @@ app.get("/api", async (req, res) => {
     }
 });
 
-// async function appStart() {
-//     const browser = await puppeteer.launch({ headless: true });
-//     const page = await browser.newPage();
-//     await page.tracing.start({
-//         categories: ["devtools.timeline"],
-//         path: "tracing.json",
-//     });
-//     await page.goto(url, { waitUntil: "load", timeout: 0 });
-//     // await page.screenshot({ path: "example.png" });
+/* 
+    const browser = await puppeteer.launch({ headless: true });
 
-//     await page.tracing.stop();
+    const page = await browser.newPage();
+    await page.tracing.start({
+        categories: ["devtools.timeline"],
+        path: "tracing.json",
+    });
+    await page.goto(url, { waitUntil: "load", timeout: 0 });
+    // await page.screenshot({ path: "example.png" });
 
-//     await browser.close();
-// }
+    await page.tracing.stop();
 
-// app.get("/start_app", async (req, res) => {
-//     try {
-//         // await fs.unlinkSync("./tracing.json");
-//         appStart();
-//         res.status(200).send({ message: "App started" });
-//     } catch (error) {
-//         res.status(404).send({ message: "Some thing went wrong", error });
-//     }
-// });
+    await browser.close();
+*/
 
-// app.get("/token", async (req, res) => {
-//     try {
-//         const events = tracingData.traceEvents.filter(
-//             (te) =>
-//                 te.name == "ResourceSendRequest" &&
-//                 te.args.data.url !== undefined
-//         );
-
-//         for await (const event of events) {
-//             let eventUrl = event.args.data.url;
-//             if (
-//                 eventUrl.includes("IntraMapsSession=") &&
-//                 !eventUrl.includes("IntraMapsSession=null")
-//             ) {
-//                 const substring = eventUrl.substring(
-//                     eventUrl.indexOf("IntraMapsSession=") + 17
-//                 );
-
-//                 return res.status(200).send({
-//                     status: true,
-//                     message: "Session getting",
-//                     token: substring,
-//                 });
-//             }
-//         }
-
-//         res.status(200).send({ status: false, message: "Session not getting" });
-//     } catch (error) {
-//         res.status(404).send({ message: "Some thing went wrong", error });
-//     }
-// });
-
-app.get("/", (req, res) => {
-    res.send({ message: "welcome" });
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server listening on port", PORT));
+module.exports = app;
