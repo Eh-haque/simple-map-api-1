1ae58e8a2464a3791220cdfce0f0c4767431c8da
Eh-haque <ehtisamul@gmail.com>
2022-10-08 17:08:15 +0600

    afjsd kaslfjasd

--- index.js
+++ index.js
@@ -1,54 +1,43 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
 const tracingData = require("./tracing.json");
-const closest_match = require("closest-match");
 const cors = require("cors");
 
-require("./jobs");
+// require("./jobs");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// async function appStart() {
-//     const browser = await puppeteer.launch({ devtools: true });
-//     const page = await browser.newPage();
-//     await page.tracing.start({
-//         categories: ["devtools.timeline"],
-//         path: "tracing.json",
-//     });
-//     await page.goto(url, { waitUntil: "load", timeout: 0 });
-//     // await page.screenshot({ path: "example.png" });
+let url =
+    "https://gisweb.casey.vic.gov.au/IntraMaps90/ApplicationEngine/frontend/mapbuilder/yourproperty.htm?configId=243fbf74-7d66-4208-899d-91b1d08ff8bf&liteConfigId=b2af2973-160e-4664-8e96-fe701aeaa67f&title=WW91ciBQcm9wZXJ0eSBhbmQgUGxhbm5pbmc%3D";
 
-//     await page.tracing.stop();
+async function appStart() {
+    const browser = await puppeteer.launch({ devtools: true });
+    const page = await browser.newPage();
+    await page.tracing.start({
+        categories: ["devtools.timeline"],
+        path: "tracing.json",
+    });
+    await page.goto(url, { waitUntil: "load", timeout: 0 });
+    // await page.screenshot({ path: "example.png" });
 
-//     await browser.close();
-// }
+    await page.tracing.stop();
 
-// app.post("/start_app", async (req, res) => {
-//     try {
-//         // await fs.unlinkSync("./tracing.json");
-//         appStart();
-//         res.status(200).send({ message: "App started" });
+    await browser.close();
+}
 
-// const browser = await puppeteer.launch({ devtools: true });
-// const page = await browser.newPage();
-// await page.tracing.start({
-//     categories: ["devtools.timeline"],
-//     path: "tracing.json",
-// });
-// await page.goto(url);
-// // await page.screenshot({ path: "example.png" });
+app.get("/start_app", async (req, res) => {
+    try {
+        // await fs.unlinkSync("./tracing.json");
+        appStart();
+        res.status(200).send({ message: "App started" });
+    } catch (error) {
+        res.status(404).send({ message: "Some thing went wrong", error });
+    }
+});
 
-// await page.tracing.stop();
-
-// await browser.close();
-//     } catch (error) {
-//         res.status(404).send({ message: "Some thing went wrong", error });
-//     }
-// });
-
-app.get("/", async (req, res) => {
+app.get("/token", async (req, res) => {
     try {
         const events = tracingData.traceEvents.filter(
             (te) =>
@@ -66,17 +55,22 @@ app.get("/", async (req, res) => {
                     eventUrl.indexOf("IntraMapsSession=") + 17
                 );
 
-                return res
-                    .status(200)
-                    .send({ message: "Session getting", token: substring });
+                return res.status(200).send({
+                    status: true,
+                    message: "Session getting",
+                    token: substring,
+                });
             }
         }
-        // events.forEach((event) => {});
 
-        res.status(200).send({ message: "Session not getting" });
+        res.status(200).send({ status: false, message: "Session not getting" });
     } catch (error) {
         res.status(404).send({ message: "Some thing went wrong", error });
     }
+});
+
+app.get("/", (req, res) => {
+    res.send({ message: "welcome" });
 });
 
 const PORT = process.env.PORT || 5000;
--- jobs.js
+++ jobs.js
@@ -1,23 +1,23 @@
-const schedule = require("node-schedule");
-const Puppeteer = require("puppeteer");
-const fs = require("fs");
+// const schedule = require("node-schedule");
+// const Puppeteer = require("puppeteer");
+// const fs = require("fs");
 
-let url =
-    "https://gisweb.casey.vic.gov.au/IntraMaps90/ApplicationEngine/frontend/mapbuilder/yourproperty.htm?configId=243fbf74-7d66-4208-899d-91b1d08ff8bf&liteConfigId=b2af2973-160e-4664-8e96-fe701aeaa67f&title=WW91ciBQcm9wZXJ0eSBhbmQgUGxhbm5pbmc%3D";
+// let url =
+//     "https://gisweb.casey.vic.gov.au/IntraMaps90/ApplicationEngine/frontend/mapbuilder/yourproperty.htm?configId=243fbf74-7d66-4208-899d-91b1d08ff8bf&liteConfigId=b2af2973-160e-4664-8e96-fe701aeaa67f&title=WW91ciBQcm9wZXJ0eSBhbmQgUGxhbm5pbmc%3D";
 
-schedule.scheduleJob("*/30 * * * *", async function () {
-    await fs.unlinkSync("./tracing.json");
+// schedule.scheduleJob("*/30 * * * *", async function () {
+//     await fs.unlinkSync("./tracing.json");
 
-    const browser = await Puppeteer.launch({ devtools: true });
-    const page = await browser.newPage();
-    await page.tracing.start({
-        categories: ["devtools.timeline"],
-        path: "tracing.json",
-    });
-    await page.goto(url, { waitUntil: "load", timeout: 0 });
-    // await page.screenshot({ path: "example.png" });
+//     const browser = await Puppeteer.launch({ devtools: true });
+//     const page = await browser.newPage();
+//     await page.tracing.start({
+//         categories: ["devtools.timeline"],
+//         path: "tracing.json",
+//     });
+//     await page.goto(url, { waitUntil: "load", timeout: 0 });
+//     // await page.screenshot({ path: "example.png" });
 
-    await page.tracing.stop();
+//     await page.tracing.stop();
 
-    await browser.close();
-});
+//     await browser.close();
+// });
