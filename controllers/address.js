97a8ecbc6cf13853b09dc7deb61dcf592064d092
Eh-haque <ehtisamul@gmail.com>
2022-10-12 12:44:38 +0600

    asdfj aksldf

--- controllers/address.js
+++ controllers/address.js
@@ -3,7 +3,8 @@ let puppeteer;
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     chrome = require("chrome-aws-lambda");
-    puppeteer = require("puppeteer-core");
+    // puppeteer = require("puppeteer-core");
+    puppeteer = require("puppeteer");
 } else {
     puppeteer = require("puppeteer");
 }
