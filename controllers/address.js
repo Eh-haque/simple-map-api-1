739a84f6204b5146c3cef2c36826ac13dbb012ae
Eh-haque <ehtisamul@gmail.com>
2022-11-17 16:41:57 +0600

    worksa

--- controllers/address.js
+++ controllers/address.js
@@ -46,7 +46,7 @@ exports.mapApi = async (req, res, next) => {
         });
 
         await page.goto(
-            'https://gisweb.casey.vic.gov.au/IntraMaps90/ApplicationEngine/frontend/mapbuilder/yourproperty.htm?configId=243fbf74-7d66-4208-899d-91b1d08ff8bf&liteConfigId=b2af2973-160e-4664-8e96-fe701aeaa67f&title=WW91ciBQcm9wZXJ0eSBhbmQgUGxhbm5pbmc%3D',
+            'https://gisweb.casey.vic.gov.au/IntraMaps22A/ApplicationEngine/frontend/mapbuilder/default.htm?configId=2c27b2b9-ee0e-49a8-9462-7085252fdff1&liteConfigId=e8c34f1d-21a3-4144-8926-a3f26ce07798&title=WW91ciUyMFByb3BlcnR5JTIwYW5kJTIwUGxhbm5pbmc=',
             { waitUntil: 'load', timeout: 0 }
         );
 
--- controllers/jobs.js
+++ controllers/jobs.js
@@ -37,63 +37,55 @@ myFunction();
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 // automated
-exports.scheduleJob = schedule.scheduleJob('*/20 * * * *', async function () {
-    try {
-        const context = await browser.createIncognitoBrowserContext();
-        const page = await context.newPage();
-        // const title = await page.title();
+exports.scheduleJob = schedule.scheduleJob(
+    '*/20 * * * *',
+    async function (req, res, next) {
+        try {
+            const context = await browser.createIncognitoBrowserContext();
+            const page = await context.newPage();
+            // const title = await page.title();
 
-        await page.tracing.start({
-            categories: ['devtools.timeline'],
-        });
+            await page.tracing.start({
+                categories: ['devtools.timeline'],
+            });
 
-        await page.goto(
-            'https://gisweb.casey.vic.gov.au/IntraMaps90/ApplicationEngine/frontend/mapbuilder/yourproperty.htm?configId=243fbf74-7d66-4208-899d-91b1d08ff8bf&liteConfigId=b2af2973-160e-4664-8e96-fe701aeaa67f&title=WW91ciBQcm9wZXJ0eSBhbmQgUGxhbm5pbmc%3D',
-            { waitUntil: 'load', timeout: 0 }
-        );
+            await page.goto(
+                'https://gisweb.casey.vic.gov.au/IntraMaps22A/ApplicationEngine/frontend/mapbuilder/default.htm?configId=2c27b2b9-ee0e-49a8-9462-7085252fdff1&liteConfigId=e8c34f1d-21a3-4144-8926-a3f26ce07798&title=WW91ciUyMFByb3BlcnR5JTIwYW5kJTIwUGxhbm5pbmc=',
+                { waitUntil: 'load', timeout: 0 }
+            );
 
-        await sleep(10000);
-        const tracing = JSON.parse(await page.tracing.stop());
+            await sleep(10000);
+            const tracing = JSON.parse(await page.tracing.stop());
 
-        // get session information
-        const events = tracing.traceEvents.filter(
-            (te) =>
-                te.name == 'ResourceSendRequest' &&
-                te.args.data.url !== undefined
-        );
+            // get session information
+            const events = tracing.traceEvents.filter(
+                (te) =>
+                    te.name == 'ResourceSendRequest' &&
+                    te.args.data.url !== undefined
+            );
 
-        for await (const event of events) {
-            let eventUrl = event.args.data.url;
-            if (
-                eventUrl.includes('IntraMapsSession=') &&
-                !eventUrl.includes('IntraMapsSession=null')
-            ) {
-                const substring = eventUrl.substring(
-                    eventUrl.indexOf('IntraMapsSession=') + 17
-                );
+            for await (const event of events) {
+                let eventUrl = event.args.data.url;
+                if (
+                    eventUrl.includes('IntraMapsSession=') &&
+                    !eventUrl.includes('IntraMapsSession=null')
+                ) {
+                    const substring = eventUrl.substring(
+                        eventUrl.indexOf('IntraMapsSession=') + 17
+                    );
 
-                const result = await Session.updateOne(
-                    { _id: '63476183533fdef5769ffe55' },
-                    { token: substring }
-                );
+                    await Session.updateOne(
+                        { _id: '63476183533fdef5769ffe55' },
+                        { token: substring }
+                    );
 
-                if (result.modifiedCount > 0) {
-                    return res.status(200).send({
-                        session: substring,
-                        result: 'session updated successfully',
-                    });
-                } else {
-                    return res.status(400).send({
-                        session: substring,
-                        result: "session isn't updated successfully",
-                    });
+                    return;
                 }
             }
-        }
 
-        await context.close();
-    } catch (error) {
-        console.error(err);
-        return res.status(400).send(error);
+            await context.close();
+        } catch (error) {
+            console.error(error);
+        }
     }
-});
+);
