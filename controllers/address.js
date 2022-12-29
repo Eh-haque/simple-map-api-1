5c45bbb41cc5a89ddca5904ff2c9e266ada96384
Shanta Miah <coder.shanta@gmail.com>
2022-12-30 02:51:42 +0600

    OOM issue resolved

--- controllers/address.js
+++ controllers/address.js
@@ -1,105 +1,90 @@
-const Session = require('../models/Session');
+const Session = require("../models/Session");
 
-let chrome = {};
 let puppeteer;
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    puppeteer = require('puppeteer');
-    // chrome = require("chrome-aws-lambda");
-    // puppeteer = require("puppeteer-core");
+  puppeteer = require("puppeteer");
+  // chrome = require("chrome-aws-lambda");
+  // puppeteer = require("puppeteer-core");
 } else {
-    puppeteer = require('puppeteer');
+  puppeteer = require("puppeteer");
 }
 
 let options = {};
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-        // args: [
-        //     ...chrome.args,
-        //     "--hide-scrollbars",
-        //     "--disable-web-security",
-        // ],
-        // defaultViewport: chrome.defaultViewport,
-        // executablePath: await chrome.executablePath,
-        // headless: true,
-        // devtools: true,
-        // ignoreHTTPSErrors: true,
-    };
+  options = {};
 }
 
 let browser;
-async function myFunction() {
-    return (browser = await puppeteer.launch());
-}
-myFunction();
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 exports.mapApi = async (req, res, next) => {
-    try {
-        const context = await browser.createIncognitoBrowserContext();
-        const page = await context.newPage();
-        // const title = await page.title();
-
-        await page.tracing.start({
-            categories: ['devtools.timeline'],
-        });
-
-        await page.goto(
-            'https://gisweb.casey.vic.gov.au/IntraMaps22A/ApplicationEngine/frontend/mapbuilder/default.htm?configId=2c27b2b9-ee0e-49a8-9462-7085252fdff1&liteConfigId=e8c34f1d-21a3-4144-8926-a3f26ce07798&title=WW91ciUyMFByb3BlcnR5JTIwYW5kJTIwUGxhbm5pbmc=',
-            { waitUntil: 'load', timeout: 0 }
-        );
-
-        await sleep(10000);
-        const tracing = JSON.parse(await page.tracing.stop());
-
-        // get session information
-        const events = tracing.traceEvents.filter(
-            (te) =>
-                te.name == 'ResourceSendRequest' &&
-                te.args.data.url !== undefined
-        );
-
-        for await (const event of events) {
-            let eventUrl = event.args.data.url;
-            if (
-                eventUrl.includes('IntraMapsSession=') &&
-                !eventUrl.includes('IntraMapsSession=null')
-            ) {
-                const substring = eventUrl.substring(
-                    eventUrl.indexOf('IntraMapsSession=') + 17
-                );
-
-                const result = await Session.updateOne(
-                    { _id: '63476183533fdef5769ffe55' },
-                    { token: substring },
-                    { new: true }
-                );
-
-                if (result.modifiedCount > 0) {
-                    return res.status(200).send({
-                        session: substring,
-                        result: 'session updated successfully',
-                    });
-                } else {
-                    return res.status(400).send({
-                        session: substring,
-                        result: "session isn't updated successfully",
-                    });
-                }
-            }
-        }
-
-        await context.close();
-
-        // res.send({
-        //     title: title,
-        //     tracing: tracing,
-        // });
-    } catch (err) {
-        console.error(err);
-        return res.status(400).send(null);
+  try {
+    if (browser === undefined) {
+      console.log("Initializing Browser...");
+      browser = await puppeteer.launch();
+    } else {
+      console.log("Re-using existing Browser...");
     }
+
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+    // const title = await page.title();
+
+    await page.tracing.start({
+      categories: ["devtools.timeline"],
+    });
+
+    await page.goto(
+      "https://gisweb.casey.vic.gov.au/IntraMaps22A/ApplicationEngine/frontend/mapbuilder/default.htm?configId=2c27b2b9-ee0e-49a8-9462-7085252fdff1&liteConfigId=e8c34f1d-21a3-4144-8926-a3f26ce07798&title=WW91ciUyMFByb3BlcnR5JTIwYW5kJTIwUGxhbm5pbmc=",
+      { waitUntil: "load", timeout: 0 }
+    );
+
+    await sleep(10000);
+    const tracing = JSON.parse(await page.tracing.stop());
+
+    // get session information
+    const events = tracing.traceEvents.filter((te) => te.name == "ResourceSendRequest" && te.args.data.url !== undefined);
+
+    await context.close();
+    await browser.close();
+    browser = undefined;
+
+    for await (const event of events) {
+      let eventUrl = event.args.data.url;
+      if (eventUrl.includes("IntraMapsSession=") && !eventUrl.includes("IntraMapsSession=null")) {
+        const substring = eventUrl.substring(eventUrl.indexOf("IntraMapsSession=") + 17);
+
+        const result = await Session.updateOne({ _id: "63476183533fdef5769ffe55" }, { token: substring }, { new: true });
+
+        if (result.modifiedCount > 0) {
+          return res.status(200).send({
+            session: substring,
+            result: "session updated successfully",
+          });
+        } else {
+          return res.status(400).send({
+            session: substring,
+            result: "session isn't updated successfully",
+          });
+        }
+      }
+    }
+
+    // res.send({
+    //     title: title,
+    //     tracing: tracing,
+    // });
+  } catch (err) {
+    console.error(err);
+
+    await context.close();
+    await browser.close();
+    browser = undefined;
+
+    return res.status(400).send(null);
+  }
 };
 
 // automated not
@@ -168,21 +153,21 @@ exports.mapApi = async (req, res, next) => {
 // );
 
 exports.getToken = async (req, res, next) => {
-    try {
-        const result = await Session.findOne({
-            _id: '63476183533fdef5769ffe55',
-        });
-        if (!result)
-            return res.status(400).send({
-                session: result,
-                result: "session isn't getting successfully",
-            });
+  try {
+    const result = await Session.findOne({
+      _id: "63476183533fdef5769ffe55",
+    });
+    if (!result)
+      return res.status(400).send({
+        session: result,
+        result: "session isn't getting successfully",
+      });
 
-        res.status(200).send({
-            session: result,
-            result: 'session getting successfully',
-        });
-    } catch (error) {
-        next({ error: error, message: 'Something went wrong' });
-    }
+    res.status(200).send({
+      session: result,
+      result: "session getting successfully",
+    });
+  } catch (error) {
+    next({ error: error, message: "Something went wrong" });
+  }
 };
--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,91 +1,84 @@
-const schedule = require('node-schedule');
-const Session = require('../models/Session');
+const schedule = require("node-schedule");
+const Session = require("../models/Session");
 
-let chrome = {};
 let puppeteer;
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    puppeteer = require('puppeteer');
-    // chrome = require("chrome-aws-lambda");
-    // puppeteer = require("puppeteer-core");
+  puppeteer = require("puppeteer");
+  // chrome = require("chrome-aws-lambda");
+  // puppeteer = require("puppeteer-core");
 } else {
-    puppeteer = require('puppeteer');
+  puppeteer = require("puppeteer");
 }
 
 let options = {};
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-        // args: [
-        //     ...chrome.args,
-        //     "--hide-scrollbars",
-        //     "--disable-web-security",
-        // ],
-        // defaultViewport: chrome.defaultViewport,
-        // executablePath: await chrome.executablePath,
-        // headless: true,
-        // devtools: true,
-        // ignoreHTTPSErrors: true,
-    };
+  options = {
+    // args: [
+    //     ...chrome.args,
+    //     "--hide-scrollbars",
+    //     "--disable-web-security",
+    // ],
+    // defaultViewport: chrome.defaultViewport,
+    // executablePath: await chrome.executablePath,
+    // headless: true,
+    // devtools: true,
+    // ignoreHTTPSErrors: true,
+  };
 }
-
-let browser;
-async function myFunction() {
-    return (browser = await puppeteer.launch());
-}
-myFunction();
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+let browser;
+
 // automated
-exports.scheduleJob = schedule.scheduleJob(
-    '*/20 * * * *',
-    async function (req, res, next) {
-        try {
-            const context = await browser.createIncognitoBrowserContext();
-            const page = await context.newPage();
-            // const title = await page.title();
-
-            await page.tracing.start({
-                categories: ['devtools.timeline'],
-            });
-
-            await page.goto(
-                'https://gisweb.casey.vic.gov.au/IntraMaps22A/ApplicationEngine/frontend/mapbuilder/default.htm?configId=2c27b2b9-ee0e-49a8-9462-7085252fdff1&liteConfigId=e8c34f1d-21a3-4144-8926-a3f26ce07798&title=WW91ciUyMFByb3BlcnR5JTIwYW5kJTIwUGxhbm5pbmc=',
-                { waitUntil: 'load', timeout: 0 }
-            );
-
-            await sleep(10000);
-            const tracing = JSON.parse(await page.tracing.stop());
-
-            // get session information
-            const events = tracing.traceEvents.filter(
-                (te) =>
-                    te.name == 'ResourceSendRequest' &&
-                    te.args.data.url !== undefined
-            );
-
-            for await (const event of events) {
-                let eventUrl = event.args.data.url;
-                if (
-                    eventUrl.includes('IntraMapsSession=') &&
-                    !eventUrl.includes('IntraMapsSession=null')
-                ) {
-                    const substring = eventUrl.substring(
-                        eventUrl.indexOf('IntraMapsSession=') + 17
-                    );
-
-                    await Session.updateOne(
-                        { _id: '63476183533fdef5769ffe55' },
-                        { token: substring }
-                    );
-
-                    return;
-                }
-            }
-
-            await context.close();
-        } catch (error) {
-            console.error(error);
-        }
+exports.scheduleJob = schedule.scheduleJob("*/20 * * * *", async function (req, res, next) {
+  try {
+    if (browser === undefined) {
+      console.log("Initializing Browser...");
+      browser = await puppeteer.launch();
+    } else {
+      console.log("Re-using existing Browser...");
     }
-);
+
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+    // const title = await page.title();
+
+    await page.tracing.start({
+      categories: ["devtools.timeline"],
+    });
+
+    await page.goto(
+      "https://gisweb.casey.vic.gov.au/IntraMaps22A/ApplicationEngine/frontend/mapbuilder/default.htm?configId=2c27b2b9-ee0e-49a8-9462-7085252fdff1&liteConfigId=e8c34f1d-21a3-4144-8926-a3f26ce07798&title=WW91ciUyMFByb3BlcnR5JTIwYW5kJTIwUGxhbm5pbmc=",
+      { waitUntil: "load", timeout: 0 }
+    );
+
+    await sleep(10000);
+    const tracing = JSON.parse(await page.tracing.stop());
+
+    // get session information
+    const events = tracing.traceEvents.filter((te) => te.name == "ResourceSendRequest" && te.args.data.url !== undefined);
+
+    await context.close();
+    await browser.close();
+    browser = undefined;
+
+    for await (const event of events) {
+      let eventUrl = event.args.data.url;
+      if (eventUrl.includes("IntraMapsSession=") && !eventUrl.includes("IntraMapsSession=null")) {
+        const substring = eventUrl.substring(eventUrl.indexOf("IntraMapsSession=") + 17);
+
+        await Session.updateOne({ _id: "63476183533fdef5769ffe55" }, { token: substring });
+
+        return;
+      }
+    }
+  } catch (error) {
+    console.error(error);
+
+    await context.close();
+    await browser.close();
+    browser = undefined;
+  }
+});
